63c85a9400718dc06faa542ddbee435f9536f848
Alvin Lee <alvin@ootbdev.com>
2025-04-16 14:00:38 -0700

    Adds scaling via Redis as backplane

--- Program.cs
+++ Program.cs
@@ -5,7 +5,40 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSignalR();
+
+var redisUrl = Environment.GetEnvironmentVariable("REDIS_URL") ?? "localhost:6379";
+
+if (redisUrl == "localhost:6379") {
+    builder.Services.AddSignalR().AddStackExchangeRedis(redisUrl, options =>
+    {
+        options.Configuration.ChannelPrefix = RedisChannel.Literal("SignalRChat");
+        options.Configuration.Ssl = redisUrl.StartsWith("rediss://");
+        options.Configuration.AbortOnConnectFail = false;
+    });
+} else {
+    var uri = new Uri(redisUrl);
+    var userInfoParts = uri.UserInfo.Split(':');
+    if (userInfoParts.Length != 2)
+    {
+        throw new InvalidOperationException("REDIS_URL is not in the expected format ('redis://user:password@host:port')");
+    }
+
+    var configurationOptions = new ConfigurationOptions
+    {
+        EndPoints = { { uri.Host, uri.Port } },
+        Password = userInfoParts[1],
+        Ssl = true,
+    };
+    configurationOptions.CertificateValidation += (sender, cert, chain, errors) => true;
+
+    builder.Services.AddSignalR(options =>
+    {
+        options.ClientTimeoutInterval = TimeSpan.FromSeconds(60); // default is 30
+        options.KeepAliveInterval = TimeSpan.FromSeconds(15);     // default is 15
+    }).AddStackExchangeRedis(redisUrl, options => {
+        options.Configuration = configurationOptions;
+    });
+}
 
 var app = builder.Build();
 
